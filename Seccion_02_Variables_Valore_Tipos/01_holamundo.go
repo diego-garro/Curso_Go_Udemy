9c50a0b5509c6d87628948a0a1e94c9b55daa2e4
diegogarromolina <diego.garromolina@ucr.ac.cr>
2020-02-28 22:30:45 -0600

    Base del repo, clase 8 terminada

---
+++ Seccion_02_Variables_Valore_Tipos/01_holamundo.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	//nb, err := fmt.Println("Hola mundo!!")
+	//_, _ = fmt.Println(nb, err)
+	x := "mi variable x"
+	fmt.Println("hola", 42, true, 55.5, x)
+}
