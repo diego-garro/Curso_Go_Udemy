afb50aba68cf5abbf45125466cfcee0701a45e42
diegogarromolina <diego.garromolina@ucr.ac.cr>
2020-03-03 23:54:12 -0600

    Clase 010 terminada

--- Seccion_02_Variables_Valore_Tipos/01_holamundo.go
+++ Seccion_02_Variables_Valore_Tipos/01_holamundo.go
@@ -6,5 +6,6 @@ func main() {
 	//nb, err := fmt.Println("Hola mundo!!")
 	//_, _ = fmt.Println(nb, err)
 	x := "mi variable x"
+	x = "Hola"
 	fmt.Println("hola", 42, true, 55.5, x)
 }
---
+++ Seccion_02_Variables_Valore_Tipos/02_palabra_var.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+var z = 41
+
+func main() {
+	x := 42 + 7
+	y := "James Bond"
+	fmt.Println(x)
+	fmt.Println(y)
+	x = 50
+	fmt.Println(x)
+	fmt.Println(z)
+	numero()
+}
+
+func numero() {
+	fmt.Println(z)
+}
