949dca2f0b97338bab5242bbfb4f5131401f777f
diegogarromolina <diego.garromolina@yahoo.com>
2020-10-15 19:50:13 -0600

    Clase 138 terminada

---
+++ Seccion_18_Concurrencia/02-race_condition.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	fmt.Println("Número de CPUs:", runtime.NumCPU())
+	fmt.Println("Número de Gorutinas:", runtime.NumGoroutine())
+	contador := 0
+
+	const gs = 100
+	var wg sync.WaitGroup
+	wg.Add(gs)
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			v := contador
+			v++
+			runtime.Gosched()
+			contador = v
+			wg.Done()
+		}()
+		fmt.Println("Número de Gorutinas:", runtime.NumGoroutine())
+	}
+	wg.Wait()
+	fmt.Println("Cuentas:", contador)
+}
